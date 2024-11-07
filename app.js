183214a5a422fa12cd04fe56abd139df7c30c6af
mahalabsi <mahatalabsi@gmail.com>
2024-11-07 08:29:49 +0300

    completed

--- app.js
+++ app.js
@@ -253,6 +253,28 @@ Solve Exercise 14 here:
 game.partyCount = () => {
   return game.party.length
 }
-
 const numberOfPokemon = game.partyCount()
 console.log('Number of Pokémon in the party:', numberOfPokemon)
+
+/*
+Exercise 15
+1. Now, complete gyms with a difficulty below 8. Reflect on how this is similar to or different from the previous gym exercises.
+(change the value of `complete` in the qualifying objects from false to true).
+
+Solve Exercise 15 here:
+*/
+game.gyms.forEach((gym) => {
+  if (gym.difficulty < 8) {
+    gym.completed = true
+  }
+})
+console.log(game.gyms)
+
+/*
+Exercise 16
+1. Log the entire `game` object to the console. Take a moment to review the changes you've made throughout the exercises.
+
+
+Solve Exercise 16 here:
+*/
+console.log(game)
